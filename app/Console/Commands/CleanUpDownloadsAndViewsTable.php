2216295e5b57846fd14745c18add45113e29b8c9
Terence Vusile Silonda <vusile@gmail.com>
2018-12-02 00:29:04 +0300

    further clean up

--- app/Console/Commands/CleanUpDownloadsAndViewsTable.php
+++ app/Console/Commands/CleanUpDownloadsAndViewsTable.php
@@ -41,17 +41,10 @@ class CleanUpDownloadsAndViewsTable extends Command
      */
     public function handle()
     {   
-        $lastMonth = Carbon::now()->subDays(30)->format('Y-m-d');
-        $query = "INSERT INTO song_downloads_archive(song_id, downloaded_on, downloads) SELECT song_id, downloaded_on, downloads from song_downloads where downloaded_on < '" . $lastMonth . "'";
-        
-        DB::SELECT($query);
+        $lastMonth = Carbon::now()->subDays(15)->format('Y-m-d');
 
         SongDownload::whereDate('downloaded_on', '<', $lastMonth)
                 ->delete();
-        
-         $query = "INSERT INTO song_views_archive(song_id, viewed_on, views) SELECT song_id, viewed_on, views from song_views where viewed_on < '" . $lastMonth . "'";
-        
-        DB::SELECT($query);
                 
         SongView::whereDate('viewed_on', '<', $lastMonth)
             ->delete();
