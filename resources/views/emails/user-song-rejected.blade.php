5829c1a59e454b80012f9e408f689b6350b2346b
Terence Vusile Silonda <vusile@gmail.com>
2018-12-03 01:16:48 +0300

    Improving

--- resources/views/emails/user-song-rejected.blade.php
+++ resources/views/emails/user-song-rejected.blade.php
@@ -6,13 +6,15 @@
     <p class="lead" style="margin: 0;padding: 0;font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;margin-bottom: 10px;font-weight: normal;font-size: 17px;line-height: 1.6;">
         Tumsifu Yesu Kristu {{ $song->user->first_name }},<br><br>
         
-        Baada ya wimbo {{$song->name}} kupitiwa na kufanyiwa uhakiki na watu watatu, matokeo ni yafuatayo. Tafadhali angalia taarifa ulizopata na uzifanyie kazi. Ili wimbo uende live, ni lazima upate walau 2/3 kwa maswali muhimu.<br><br>
+        Baada ya wimbo {{$song->name}} kupitiwa na kufanyiwa uhakiki na watu watatu, matokeo ni yafuatayo. Tafadhali angalia taarifa ulizopata na uzifanyie kazi. Ili wimbo uende live, ni lazima upate walau 2/3 kwa maswali muhimu. Umefaulu maswali mengine yote isipokuwa yafuatayo:<br><br>
         
         @foreach($approvalQuestionScores as $approvalQuestion)
-            <p>
-                {!! $approvalQuestion->question !!};<br>
-                <strong>Umepata {{config('song.reviews.no_of_reviews_per_song') - $approvalQuestion->answers_count}} / {{config('song.reviews.no_of_reviews_per_song')}} </strong>
-            </p>
+            @if(config('song.reviews.no_of_reviews_per_song') - $approvalQuestion->answers_count < 2)
+                <p>
+                    {!! $approvalQuestion->question !!};<br>
+                    <strong>Umepata {{config('song.reviews.no_of_reviews_per_song') - $approvalQuestion->answers_count}} / {{config('song.reviews.no_of_reviews_per_song')}} </strong>
+                </p>
+            @endif
         @endforeach
         
         Tafadhali fanya marekebisho kama yalivyoelezwa.<br><br>
