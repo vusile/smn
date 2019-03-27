1d225792b29d128b2519eec0c7909e0c5187cc3b
Terence Vusile Silonda <vusile@gmail.com>
2019-03-28 01:03:11 +0300

    Fixes

--- resources/views/users/partials/account-user-row.blade.php
+++ resources/views/users/partials/account-user-row.blade.php
@@ -9,20 +9,20 @@
         @if($user->hasAnyRole(['uhakiki', 'ithibati', 'admin', 'super admin']))
             <p>
                 @if($user->hasRole('uhakiki'))
-                    <a href = 'remove-role/uhakiki/{{$user->id}}'>Ondoa uwezo wa Kuhakiki</a><br>
+                    <a href = '/remove-role/uhakiki/{{$user->id}}'>Ondoa uwezo wa Kuhakiki</a><br>
                 @endif
                 @if($user->hasRole('ithibati'))
-                    <a href = 'remove-role/ithibati/{{$user->id}}'>Ondoa uwezo wa Kutoa Ithibati</a><br>
+                    <a href = '/remove-role/ithibati/{{$user->id}}'>Ondoa uwezo wa Kutoa Ithibati</a><br>
                 @endif
                 @if($user->hasRole('admin'))
-                    <a href = 'remove-role/admin/{{$user->id}}'>Ondoa uwezo wa Kuwa Admin</a><br>
+                    <a href = '/remove-role/admin/{{$user->id}}'>Ondoa uwezo wa Kuwa Admin</a><br>
                 @endif
             </p>
         @else
             <p>
-                <a href = 'assign-role/uhakiki/{{$user->id}}'>Mpe uwezo wa Kuhakiki</a><br>
-                <a href = 'assign-role/ithibati/{{$user->id}}'>Mpe uwezo wa Kutoa Ithibati</a><br>
-                <a href = 'assign-role/admin/{{$user->id}}'>Mpe uwezo wa Kuwa Admin</a><br>
+                <a href = '/assign-role/uhakiki/{{$user->id}}'>Mpe uwezo wa Kuhakiki</a><br>
+                <a href = '/assign-role/ithibati/{{$user->id}}'>Mpe uwezo wa Kutoa Ithibati</a><br>
+                <a href = '/assign-role/admin/{{$user->id}}'>Mpe uwezo wa Kuwa Admin</a><br>
             </p>
         @endif
     </div>
