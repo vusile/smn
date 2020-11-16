08663a0cb0acd144f3d7ce26a3dcb9139ca36bc4
Terence Vusile Silonda <vusile@gmail.com>
2020-11-16 22:45:14 +0300

    Bug fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,31 +17,32 @@ class SearchController extends Controller
      * @var SongService
      */
     protected $songService;
-    
+
     /*
      * @var SearchService
      */
     protected $searchService;
-    
+
     public function __construct(
         SearchService $searchService
     ) {
         $this->searchService = $searchService;
     }
-    
+
     public function index()
     {
         $composersTotal = 0;
         $songsTotal = 0;
+        $composerNames = [];
         $composers = $this->searchService
             ->search(request()->query('st'), 'composers');
-        
+
         if($composers) {
             $composers = Composer::whereIn(
                 'id',
                 $composers->pluck('id')->toArray()
             )->get();
-            
+
             $composersTotal = $composers->count();
         }
 
@@ -52,16 +53,16 @@ class SearchController extends Controller
             $songs = $songs->filter(function($song){
                 return in_array($song->status, [1,2,7,8]);
             });
-            
+
             $composerNames = Composer::whereIn(
                     'id',
                     $songs->pluck('composer_id')->toArray()
                 )
                 ->pluck('name', 'id');
-            
+
             $songsTotal = $songs->count();
         }
-        
+
         return view(
             'search.index',
             compact(
@@ -73,20 +74,20 @@ class SearchController extends Controller
             )
         );
     }
-    
+
     public function searchUserSongs()
     {
         $songs = $this->searchService
             ->userSearch(request()->query('q'), 'songs');
-        
+
         $status = 'Nyimbo Ulizotafuta';
-        
+
         $songs = Song::whereIn(
                 'id',
                 $songs->pluck('id')->toArray()
             )
                 ->paginate(20);
-        
+
         return view(
             'account.songs',
             compact('songs', 'status')
