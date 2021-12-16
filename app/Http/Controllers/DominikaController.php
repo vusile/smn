35390c001e5bd2dab3ca551a1bac5ad5d8cdca4a
Terence Vusile Silonda <vusile@gmail.com>
2021-12-16 17:00:44 +0300

    Hide in DOminikas

--- app/Http/Controllers/DominikaController.php
+++ app/Http/Controllers/DominikaController.php
@@ -10,29 +10,30 @@ use Illuminate\Support\Carbon;
 
 class DominikaController extends Controller
 {
-    
+
     public function index()
     {
         $title = "Nyimbo za Dominika na Sikukuu Zilizoamriwa";
         $description = "Pata Nyimbo za Dominika na Sikukuu Zilizoamriwa";
         SEOMeta::setTitle($title);
         SEOMeta::setDescription($description);
-        
+
         $dominikas = Dominika::where(
                 function($query) {
                     $query->whereIn('year_id', config('dominika.mwaka'))
                             ->orWhere('year_id', null);
                 }
-                
+
             )
             ->whereDate('dominika_date', '>=', Carbon::today()->toDateString())
             ->orderBy('dominika_date')
             ->get();
-        
+
         $ibadaZaWikiHii = Dominika::thisWeek()
+            ->has('songs')
             ->orderBy('dominika_date')
             ->get();
-       
+
         return view(
             'dominika.index',
             compact(
@@ -43,28 +44,28 @@ class DominikaController extends Controller
             )
         );
     }
-    
+
     public function show(string $url, Dominika $dominika)
     {
         SEOMeta::setTitle($dominika->title);
         SEOMeta::setDescription(
             'Nyimbo za ' . $dominika->title
         );
-        
+
         $dominikaSongs = DB::table('dominikas_songs')
                 ->where('dominika_id', $dominika->id)
                 ->get();
-        
+
         $mwanzo = $dominikaSongs
                 ->where('parts_of_mass_id', 1)
                 ->pluck('song_id')
                 ->all();
-        
+
         $katikati = $dominikaSongs
                 ->where('parts_of_mass_id', 2)
                 ->pluck('song_id')
                 ->all();
-        
+
         $shangilio = $dominikaSongs
                 ->where('parts_of_mass_id', 3)
                 ->pluck('song_id')
@@ -74,15 +75,15 @@ class DominikaController extends Controller
                 ->where('parts_of_mass_id', 4)
                 ->pluck('song_id')
                 ->all();
-        
-        
+
+
         $approvedDominikaSongs = Song::approved()
                 ->select('id', 'name', 'url', 'views', 'downloads', 'composer_id')
                 ->with('composer:id,name')
                 ->whereIn('id', $dominikaSongs->pluck('song_id')->all())
                 ->orderBy('views')
                 ->get();
-         
+
         return view(
             'dominika.show',
             compact(
