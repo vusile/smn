fb08b8e5076b3c0b8432c1b7e52fc221d35db440
Terence Vusile Silonda <vusile@gmail.com>
2022-09-09 10:42:22 +0300

    Debug

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -24,8 +24,6 @@ class AccountController extends Controller
             ->groupBy('user_id')
             ->get();
 
-        dd($songsReviewed);
-
         return view(
             'account.index',
             compact(
