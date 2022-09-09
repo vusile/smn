3b6a2fa9147e7c4d6cadd49963a74159f6d8b3bf
Terence Vusile Silonda <vusile@gmail.com>
2022-09-09 11:08:57 +0300

    Debug

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -3,15 +3,15 @@
 <div class="notika-status-area">
         <div class="container">
             <div class="row">
-{{--                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">--}}
-{{--                    <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">--}}
-{{--                        <div class="website-traffic-ctn">--}}
-{{--                            <h2><span class="counter">{{number_format($pendingSongs + $activeSongs)}}</span></h2>--}}
-{{--                            <p>Nyimbo Ulizoupload</p>--}}
-{{--                        </div>--}}
-{{--                        <div class="sparkline-bar-stats1">9,4,8,6,5,6,4,8,3,5,9,5</div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                    <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
+                        <div class="website-traffic-ctn">
+                            <h2><span class="counter">{{number_format($pendingSongs + $activeSongs)}}</span></h2>
+                            <p>Nyimbo Ulizoupload</p>
+                        </div>
+                        <div class="sparkline-bar-stats1">9,4,8,6,5,6,4,8,3,5,9,5</div>
+                    </div>
+                </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
                         <div class="website-traffic-ctn">
