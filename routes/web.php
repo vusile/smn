25f539af578eaba3cacbe0efc26339166f8803de
Terence Vusile Silonda <vusile@gmail.com>
2023-03-15 01:54:00 +0300

    Clean up routes

--- routes/web.php
+++ routes/web.php
@@ -20,45 +20,45 @@ Route::get('/home', function() {
 });
 Route::get('/blog/{slug}/{blogPost}', 'BlogPostController@show');
 Route::get('/blog', 'BlogPostController@index');
-//Route::get('/song/{slug}/{song}', function($slug, $song){
-//    return redirect("/wimbo/$slug/$song", 301);
-//});
+Route::get('/song/{slug}/{song}', function($slug, $song){
+    return redirect("/wimbo/$slug/$song", 301);
+});
 Route::get('/wimbo/{slug}/{song}', 'SongController@show')
         ->middleware(['check_song_status', 'check_song_url']);
-//Route::get('/composer/profile/{slug}/{composer}', function($slug, $composer){
-//    return redirect("/watunzi/wasifu-mawasiliano/$slug/$composer", 301);
-//});
+Route::get('/composer/profile/{slug}/{composer}', function($slug, $composer){
+    return redirect("/watunzi/wasifu-mawasiliano/$slug/$composer", 301);
+});
 Route::get('/watunzi/wasifu-mawasiliano/{slug}/{composer}', 'ComposerController@show');
-//Route::get('/composer/songs/{slug}/{composer}', function ($slug, $composer) {
-//    return redirect("/watunzi/nyimbo/$slug/$composer", 301);
-//});
+Route::get('/composer/songs/{slug}/{composer}', function ($slug, $composer) {
+    return redirect("/watunzi/nyimbo/$slug/$composer", 301);
+});
 Route::get('/watunzi/nyimbo/{slug}/{composer}', 'ComposerController@songs');
 Route::get('/watunzi/', 'ComposerController@index');
-//Route::get('/uploaders/', function() {
-//    return redirect('/wapakia-nyimbo', 301);
-//});
+Route::get('/uploaders/', function() {
+    return redirect('/wapakia-nyimbo', 301);
+});
 Route::get('/wapakia-nyimbo/', 'UserController@index');
-//Route::get('/uploader/songs/{slug}/{user}', function ($slug, $user){
-//    return redirect("/wapakia-nyimbo/$slug/$user", 301);
-//});
+Route::get('/uploader/songs/{slug}/{user}', function ($slug, $user){
+    return redirect("/wapakia-nyimbo/$slug/$user", 301);
+});
 Route::get('/wapakia-nyimbo/{slug}/{user}', 'UserController@songs');
 Route::get('/song/download/{song}/{type}/{name}', 'SongController@download');
-//Route::get('/categories', function(){
-//    return redirect('/makundi-nyimbo', 301);
-//});
+Route::get('/categories', function(){
+    return redirect('/makundi-nyimbo', 301);
+});
 Route::get('/makundi-nyimbo', 'CategoryController@index');
 Route::get('/makundi-nyimbo/{slug}/{category}', 'CategoryController@show')
         ->middleware('check_category_url');
-//Route::get('/category/{slug}/{category}', function($slug, $category) {
-//    return redirect("/makundi-nyimbo/$slug/$category", 301);
-//});
-//Route::get('/nyimbozajumapilinasikukuu/', function () {
-//    return redirect('/dominika-sikukuu', 301);
-//});
+Route::get('/category/{slug}/{category}', function($slug, $category) {
+    return redirect("/makundi-nyimbo/$slug/$category", 301);
+});
+Route::get('/nyimbozajumapilinasikukuu/', function () {
+    return redirect('/dominika-sikukuu', 301);
+});
 Route::get('/dominika-sikukuu/', 'DominikaController@index');
-//Route::get('/nyimbozajumapilinasikukuu/ratiba/{slug}/{dominika}', function ($slug, $dominika) {
-//    return redirect("/dominika-sikukuu/$slug/$dominika", 301);
-//});
+Route::get('/nyimbozajumapilinasikukuu/ratiba/{slug}/{dominika}', function ($slug, $dominika) {
+    return redirect("/dominika-sikukuu/$slug/$dominika", 301);
+});
 Route::get('/dominika-sikukuu/{slug}/{dominika}', 'DominikaController@show');
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
@@ -205,8 +205,8 @@ Route::post('/contact', 'ContactController@sendEMail')->name('send-message');
 Route::get('/sitemap.xml', 'SitemapController@index')->name('sitemap');
 Route::get('/wwh', 'WebHookController@verify')->name('wwh-verify');
 Route::post('/wwh', 'WebHookController@event')->name('wwh-event');
-//Route::get('/privacy-policy', function() {
-//    return view(
-//        'privacy-policy'
-//    );
-//});
+Route::get('/privacy-policy', function() {
+    return view(
+        'privacy-policy'
+    );
+});
