1d52fda5bce6589915df12e9b53edd02f93fa779
Terence Vusile Silonda <vusile@gmail.com>
2023-04-16 19:00:44 +0300

    Revert

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $smsService = new SmsService();
-        $smsService->sendOptions();
+//        $smsService = new SmsService();
+//        $smsService->sendOptions();
 
         $now = Carbon::now();
 
