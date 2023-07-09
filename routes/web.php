934b9231d2d7498fa32a77dd1d19fcb77d47186c
Terence Vusile Silonda <vusile@gmail.com>
2023-07-09 13:36:56 +0300

    Protect routes

--- routes/web.php
+++ routes/web.php
@@ -210,12 +210,11 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/admin/dominikas/update-songs/{dominika}', 'Admin\DominikaController@update_songs')->name('admin-doninika-update-songs');
     });
 
-    Route::get('/admin/upload-donor-report', 'DonorsController@upload');
-    Route::get('/admin/mkeka', 'DonorsController@mkeka');
-    Route::get('/admin/tuma-mkeka', 'DonorsController@tumaMkeka');
     Route::group(['middleware' => ['role:donors']], function () {
+        Route::get('/admin/tuma-mkeka', 'DonorsController@tumaMkeka');
+        Route::get('/admin/mkeka', 'DonorsController@mkeka');
+        Route::get('/admin/upload-donor-report', 'DonorsController@upload');
     });
-
 
     Route::post('/delete/{song}', 'SongUploadController@delete')
         ->name('delete-song')
