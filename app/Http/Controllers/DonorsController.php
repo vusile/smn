3f64048533f487becd6bb849017df0c23985c92f
Terence Vusile Silonda <vusile@gmail.com>
2023-07-10 11:06:26 +0300

    Hide cumulative for current month

--- app/Http/Controllers/DonorsController.php
+++ app/Http/Controllers/DonorsController.php
@@ -46,14 +46,19 @@ class DonorsController extends Controller
 
     public function generateWhatsappMessage($monthlyTotals, $totals, $monthlyTotal, $donors, $date)
     {
-
+        $currentDate = Carbon::now();
+        $hideCumulative = $currentDate->format('Y-m') == $date->format('Y-m');
         $message = whatsappBold(
             sprintf('TUMSIFU YESU KRISTO*.<br><br>*Jumla ya michango mwezi wa %s %s: %s', $date->monthName, $date->year, number_format($monthlyTotal))
         );
 
-        $message .= '<br><br>_Maelezo: Namba iliyo baada ya jina lako, ni mchango wa mwezi huu. Namba iliyo kwenye mabano ni jumla ya michango yako tangia zoezi lianze_';
-        $message .= '<br><br>';
-        $message .= '_Mfano: Francis John - 2,000 (10,000). 2,000 ni mchango wa mwezi huu. 10,000 ni jumla uliyochanga (ikijumuisha hiyo 2,000) tangu tuanze kuchangia._';
+        if(!$hideCumulative)
+        {
+            $message .= '<br><br>_Maelezo: Namba iliyo baada ya jina lako, ni mchango wa mwezi huu. Namba iliyo kwenye mabano ni jumla ya michango yako tangia zoezi lianze_';
+            $message .= '<br><br>';
+            $message .= '_Mfano: Francis John - 2,000 (10,000). 2,000 ni mchango wa mwezi huu. 10,000 ni jumla uliyochanga (ikijumuisha hiyo 2,000) tangu tuanze kuchangia._';
+        }
+
         $message .= '<br><br>Sponsors waliochangia mwezi wa ' . $date->monthName . ' ' . $date->year . ' kupitia M-Koba (SMN SPONSORS) na CHANGISHA NUMBER 8182296 (VUSILE SILONDA SMN SPONSORS) Ni:';
         $message .= '<br><br>*_Kama kuna kasoro yoyote tafadhali tufahamishe_*';
         $message .= '<br><br>';
@@ -63,9 +68,12 @@ class DonorsController extends Controller
         foreach ($donors as $donor) {
             $message .= $index . ". " . $donor->name . " - ";
             $message .= isset($monthlyTotals[$donor->id]) ? number_format($monthlyTotals[$donor->id]) : 0;
-            $message .= " (";
-            $message .= isset($totals[$donor->id]) ? number_format($totals[$donor->id]) : 0 ;
-            $message .= ')<br>';
+            if(!$hideCumulative) {
+                $message .= " (";
+                $message .= isset($totals[$donor->id]) ? number_format($totals[$donor->id]) : 0 ;
+                $message .= ')';
+            }
+            $message .= '<br>';
             $index += 1;
         }
 
