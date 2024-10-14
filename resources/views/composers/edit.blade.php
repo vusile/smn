49aae747d1b1e8fdc362c4aacff568e581ddb7a7
Vusile Terence Silonda <vusile@gmail.com>
2024-10-14 18:03:41 +0300

    summernote https

--- resources/views/composers/edit.blade.php
+++ resources/views/composers/edit.blade.php
@@ -110,8 +110,8 @@
 </div>
 @section('footer')
     <!-- include summernote css/js -->
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
     <script>
         $(document).ready(function() {
            $('#summernote').summernote({
